5bb8080366062d7f70ae053789c99cc7012ba126
Sivakkanth <sivakanth2000s@gmail.com>
2024-06-02 01:53:52 +0530

    Finished

--- main.js
+++ main.js
@@ -5,3 +5,34 @@ var typed = new Typed(".text", {
     backDelay:1000,
     loop:true
 });
+
+function toggleMenu() {
+    const navLinks = document.getElementById('mini-navbar');
+    if (navLinks.classList.contains('hide')) {
+        navLinks.classList.remove('hide');
+        navLinks.classList.add('show');
+    } else {
+        navLinks.classList.remove('show');
+        navLinks.classList.add('hide');
+    }
+}
+
+function hideMenu() {
+    const navLinks = document.getElementById('mini-navbar');
+    navLinks.classList.remove('show');
+    navLinks.classList.add('hide');
+}
+
+window.onload = function() {
+    const navLinks = document.getElementById('mini-navbar');
+    navLinks.classList.add('hide');
+}
+
+const toTop = document.querySelector('.to-top');
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    }else{
+        toTop.classList.remove("active")
+    }
+})
