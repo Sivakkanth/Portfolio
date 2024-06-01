ca4cc35e87948a9dbe0654f083e3e0782660e01e
Sivakkanth <sivakanth2000s@gmail.com>
2024-06-01 17:00:14 +0530

    Main function finish

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["a Front End Developer", "a Mobile App Developer", "an UI/UX Designer"],
+    typeSpeed:100,
+    backSpeed:20,
+    backDelay:1000,
+    loop:true
+});
